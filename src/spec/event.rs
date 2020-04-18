01c25d8d36e65945b65a65b108a67ffe3627df81
Dowland Aiello <mitsukomegumii@gmail.com>
2020-04-18 15:34:42 -0400

    src/spec/event: Implement PrivMessage according to event.capnp.

--- src/spec/event.rs
+++ src/spec/event.rs
@@ -36,3 +36,65 @@ impl Message {
         &self.data
     }
 }
+
+/// PrivMessage is a message sent as text, rendered on the client corresponding
+/// to the user that the message is targeting
+pub struct PrivMessage {
+    /// The username of the chatter that the message will be sent to
+    concerns: String,
+
+    /// The contents of the private message
+    message: Message,
+}
+
+impl PrivMessage {
+    /// Creates a new owned private message.
+    ///
+    /// # Arguments
+    ///
+    /// * `to` - A string representing the username of the recipient of this
+    /// message
+    /// * `contents` - A string representing the contents of the message
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use gnomegg::spec::event::PrivMessage;
+    ///
+    /// let msg = PrivMessage::new("essaywriter".to_owned(), "I have information concerning the murder of Jeffrey Epstein.".to_owned());
+    /// ```
+    pub fn new(to: String, contents: String) -> Self {
+        Self {
+            concerns: to,
+            message: Message::new(contents),
+        }
+    }
+
+    /// Retreives the username of the recipient of this message.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use gnomegg::spec::event::PrivMessage;
+    ///
+    /// let msg = PrivMessage::new("essaywriter".to_owned(), "I have information concerning the murder of Jeffrey Epstein.".to_owned());
+    /// msg.to(); // => "essaywriter"
+    /// ```
+    pub fn to(&self) -> &str {
+        &self.concerns
+    }
+
+    /// Retrieves the contents of the private message.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use gnomegg::spec::event::PrivMessage;
+    ///
+    /// let msg = PrivMessage::new("essaywriter".to_owned(), "I have information concerning the murder of Jeffrey Epstein.".to_owned());
+    /// msg.contents(); // => "I have information concerning the murder of Jeffrey Epstein."
+    /// ```
+    pub fn contents(&self) -> &str {
+        self.message.contents()
+    }
+}
