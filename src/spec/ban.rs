07d897f466e3997d353aac51aaaaa9389cd9f5df
Dowland Aiello <mitsukomegumii@gmail.com>
2020-04-26 14:59:59 -0400

    spec: Create ban spec.

--- src/spec/ban.rs
+++ src/spec/ban.rs
@@ -1,3 +1,13 @@
+use super::user::User;
+use chrono::{DateTime, NaiveDateTime, Utc};
+
+/// Ban represents a ban entry in the SQL database.
+#[derive(
+    Identifiable, Insertable, Queryable, Associations, Serialize, Deserialize, PartialEq, Debug,
+)]
+#[belongs_to(User)]
+#[table_name = "bans"]
+#[primary_key(user_id)]
 pub struct Ban {
     /// The ID of the user corresponding to this ban
     user_id: u64,
@@ -5,7 +15,102 @@ pub struct Ban {
     /// The (optional) number of nanoseconds that this ban will be in effect for
     duration: Option<u64>,
 
+    /// The time at which the ban was issued
     initiated_at: NaiveDateTime,
 
-    ip: Option<String>
+    /// The IP address of the user being banned
+    ip: Option<String>,
+}
+
+impl Default for Ban {
+    fn default() -> Self {
+        Self {
+            user_id: 0,
+            duration: None,
+            initiated_at: Utc::now().naive_utc(),
+        }
+    }
+}
+
+impl Ban {
+    /// Creates a new ban primitive, assuming a permaban at the current time.
+    pub fn new(user_id: u64) -> Self {
+        Self {
+            user_id,
+            duration: None,
+            initiated_at: Utc::now().naive_utc(),
+            ip: None,
+        }
+    }
+
+    /// Creates a new ban primitive based off the current ban instance, with
+    /// the provided user ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `user_id` - The user ID that should be associated with the ban
+    pub fn with_user_id(mut self, user_id: u64) -> Self {
+        self.user_id = user_id;
+
+        self
+    }
+
+    /// Creates a new ban primitive based off the current ban instance, with
+    /// the provided duration (in nanoseconds).
+    ///
+    /// # Arguments
+    ///
+    /// * `duration` - The number of nanoseconds that the ban should be active
+    /// for
+    pub fn with_duration(mut self, duration: u64) -> Self {
+        self.duration = Some(duration);
+
+        self
+    }
+
+    /// Creates a new ban primitive based off the current ban instance, with
+    /// the provided initiation time (UTC).
+    ///
+    /// # Arguments
+    ///
+    /// * `initiated_at` - The time at which the ban was issued
+    pub fn with_initiation_timestamp(mut self, initiated_at: DateTime<Utc>) -> Self {
+        self.initiated_at = initiated_at.naive_utc();
+
+        self
+    }
+
+    /// Creates a new ban primitive based off the current ban instance, with
+    /// the provided IP.
+    ///
+    /// # Arguments
+    ///
+    /// * `ip` - The IP of the user being banned
+    pub fn with_ip(mut self, ip: String) -> Self {
+        self.ip = Some(ip);
+
+        self
+    }
+
+    /// Determines whether or not the ban is active.
+    pub fn active(&self) -> bool {
+        self.active_for()
+            .map_or(true, |d| Utc::now().naive_utc() < self.initiated_at + d)
+    }
+
+    /// Retreieves the ID pertaining to the use who will be band.
+    pub fn concerns(&self) -> u64 {
+        self.user_id
+    }
+
+    /// Constructs a duration representing the timeframe that the ban will be
+    /// active for.
+    pub fn active_for(&self) -> Option<Duration> {
+        self.duration.map(|d| Duration::nanoseconds(d as i64))
+    }
+
+    /// Obtains the IP adddress of the user being banned.
+    pub fn address(&self) -> Option<&str> {
+        self.ip.map(|raw| &raw)
+    }
 }
