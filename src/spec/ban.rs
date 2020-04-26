5b23736ed1c701838441eef9f6e92cf29b9cb46d
Dowland Aiello <mitsukomegumii@gmail.com>
2020-04-26 15:22:23 -0400

    spec/ban: Write a NewBan adapter.

--- src/spec/ban.rs
+++ src/spec/ban.rs
@@ -1,5 +1,7 @@
-use super::user::User;
-use chrono::{DateTime, NaiveDateTime, Utc};
+use super::{schema::bans, user::User};
+use chrono::{DateTime, Duration, NaiveDateTime, Utc};
+use diesel::Associations;
+use serde::{Deserialize, Serialize};
 
 /// Ban represents a ban entry in the SQL database.
 #[derive(
@@ -28,6 +30,7 @@ impl Default for Ban {
             user_id: 0,
             duration: None,
             initiated_at: Utc::now().naive_utc(),
+            ip: None,
         }
     }
 }
@@ -111,6 +114,69 @@ impl Ban {
 
     /// Obtains the IP adddress of the user being banned.
     pub fn address(&self) -> Option<&str> {
-        self.ip.map(|raw| &raw)
+        self.ip.as_deref()
+    }
+}
+
+/// NewBan represents a request to add a ban entry in the database.
+#[derive(Insertable, Serialize, Deserialize, PartialEq, Debug)]
+#[table_name = "bans"]
+pub struct NewBan<'a> {
+    /// The ID of the user corresponding to this ban
+    user_id: u64,
+
+    /// The (optional) number of nanoseconds that this ban will be in effect for
+    duration: Option<u64>,
+
+    /// The time at which the ban was issued
+    initiated_at: NaiveDateTime,
+
+    /// The IP address of the user being banned
+    ip: Option<&'a str>,
+}
+
+impl<'a> NewBan<'a> {
+    /// Creates a new request to add a ban entry in the database.
+    ///
+    /// # Arguments
+    ///
+    /// * `user_id` - The ID of the user corresponding to this ban
+    /// * `duration` - The (optional) number of nanoseconds that this ban will be in effect for
+    /// * `initiated_at` - The time at which the ban was issued
+    /// * `ip` - The (optional) IP address of the user being banned
+    pub fn new(
+        user_id: u64,
+        duration: Option<u64>,
+        initiated_at: NaiveDateTime,
+        ip: Option<&'a str>,
+    ) -> Self {
+        Self {
+            user_id,
+            duration,
+            initiated_at,
+            ip,
+        }
+    }
+
+    /// Determines whether or not the ban is active.
+    pub fn active(&self) -> bool {
+        self.active_for()
+            .map_or(true, |d| Utc::now().naive_utc() < self.initiated_at + d)
+    }
+
+    /// Retreieves the ID pertaining to the use who will be band.
+    pub fn concerns(&self) -> u64 {
+        self.user_id
+    }
+
+    /// Constructs a duration representing the timeframe that the ban will be
+    /// active for.
+    pub fn active_for(&self) -> Option<Duration> {
+        self.duration.map(|d| Duration::nanoseconds(d as i64))
+    }
+
+    /// Obtains the IP adddress of the user being banned.
+    pub fn address(&self) -> Option<&str> {
+        self.ip
     }
 }
