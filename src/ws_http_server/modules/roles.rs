b2b38fd732191328955b44a44061cac55a0d4015
Dowland Aiello <mitsukomegumii@gmail.com>
2020-05-03 11:02:27 -0400

    http/modules/roles: Start working on a provider for the roles table in the persisted database.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

---
+++ src/ws_http_server/modules/roles.rs
@@ -0,0 +1,6 @@
+/// Provider represents an arbitrary provider of the roles lib API.
+/// The roles API is responsible for managing roles corresponding to certain
+/// users.
+pub trait Provider {
+
+}
